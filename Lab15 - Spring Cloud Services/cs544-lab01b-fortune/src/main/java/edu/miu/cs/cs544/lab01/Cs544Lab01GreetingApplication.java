6aa1ab7d093367ecd277bc708f03003bd9eb6172
Hedra <Hedra_gerges@live.com>
2020-03-13 01:09:40 -0600

    add a service discovery server

--- Lab15 - Spring Cloud Services/cs544-lab01b-fortune/src/main/java/edu/miu/cs/cs544/lab01/Cs544Lab01GreetingApplication.java
+++ Lab15 - Spring Cloud Services/cs544-lab01b-fortune/src/main/java/edu/miu/cs/cs544/lab01/Cs544Lab01GreetingApplication.java
@@ -2,7 +2,14 @@ package edu.miu.cs.cs544.lab01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+//@EnableDiscoveryClient
+
+@EnableEurekaClient
 @SpringBootApplication
 public class Cs544Lab01GreetingApplication {
 
